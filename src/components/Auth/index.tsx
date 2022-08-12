43ffbc7e0cc294dfb266edeb4f50aa72fda900c2
Idris Akintobi <akintobiidris@gmail.com>
2022-08-12 09:22:30 +0100

    created auth index

---
+++ src/components/Auth/index.tsx
@@ -0,0 +1,25 @@
+import { Center, Container, Heading, Button } from "@chakra-ui/react";
+import { useState } from "react";
+import SignUpForm from "./SignUpForm";
+import SignInForm from "./SignInForm";
+
+const Auth = () => {
+  const [signIn, setSignIn] = useState(true);
+  return (
+    <>
+      <Center marginTop={40}>
+        <Container>
+          <Heading textAlign={"left"} marginBottom={8}>
+            {signIn ? "Sign in" : "Sign up"}
+          </Heading>
+          {signIn ? <SignInForm /> : <SignUpForm />}
+          <Button onClick={() => setSignIn(!signIn)} marginTop={8}>
+            {signIn ? "Don't have an account?" : "Already a member?"}
+          </Button>
+        </Container>
+      </Center>
+    </>
+  );
+};
+
+export default Auth;
