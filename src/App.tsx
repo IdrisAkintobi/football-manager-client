2a5dc31c23065b77b350ac1694d2cfe81507849f
Idris Akintobi <akintobiidris@gmail.com>
2022-09-25 22:07:36 +0100

    app first run

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Auth from "./components/Auth";
-const App = () => (
-  <Router>
-    <Routes>
-      <Route path="/" element={<Auth />} />
-    </Routes>
-  </Router>
-);
+import Dashboard from "./components/Dashboard/Dashboard";
+import Market from "./components/Market/Market";
+import { Provider } from "react-redux";
+import { store, persistor } from "./redux/store";
+import Nav from "./components/Header/Header";
+import { PersistGate } from "redux-persist/integration/react";
+import { Loading } from "./components/Loading/Loading";
+import Protected from "./components/Protected/Protected";
 
-export default App;
+export const App = () => (
+  <Provider store={store}>
+    <PersistGate loading={<Loading />} persistor={persistor}>
+      <Nav />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Auth />} />
+          <Route element={<Protected />}>
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/market" element={<Market />} />
+          </Route>
+        </Routes>
+      </Router>
+    </PersistGate>
+  </Provider>
+);
