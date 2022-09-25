6020b103791a2823f4df5f461a60d978be8e45ab
Idris Akintobi <akintobiidris@gmail.com>
2022-09-25 22:11:29 +0100

    Added stats for value and budgets

---
+++ src/components/Stat/Stats.tsx
@@ -0,0 +1,36 @@
+import {
+  Stat,
+  StatLabel,
+  StatNumber,
+  StatArrow,
+  StatGroup,
+  Flex,
+} from "@chakra-ui/react";
+interface propType {
+  budget: number;
+  value: number;
+}
+const Stats = ({ budget, value }: propType) => {
+  return (
+    <Flex justifyContent={"end"} mr={"16"}>
+      <StatGroup gap={"4"}>
+        <Stat>
+          <StatLabel>Value</StatLabel>
+          <StatNumber>${value}</StatNumber>
+          <StatArrow
+            type={value < 20000000 ? "decrease" : "increase"}
+          ></StatArrow>
+        </Stat>
+        <Stat>
+          <StatLabel>Budget</StatLabel>
+          <StatNumber>${budget}</StatNumber>
+          <StatArrow
+            type={budget < 5000000 ? "decrease" : "increase"}
+          ></StatArrow>
+        </Stat>
+      </StatGroup>
+    </Flex>
+  );
+};
+
+export default Stats;
