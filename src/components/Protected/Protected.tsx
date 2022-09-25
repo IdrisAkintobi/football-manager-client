3aa57540ac9d38f0526bb8c1b518e0f6aca3008f
Idris Akintobi <akintobiidris@gmail.com>
2022-09-25 22:10:50 +0100

    Added protected route

---
+++ src/components/Protected/Protected.tsx
@@ -0,0 +1,10 @@
+import { Outlet, Navigate } from "react-router";
+import { useAppSelector } from "../../redux/hooks";
+import { selectToken } from "../../redux/reducers/user.slice";
+
+const Protected = () => {
+  const token = useAppSelector(selectToken);
+  return !token ? <Navigate to={"/"} /> : <Outlet />;
+};
+
+export default Protected;
